e73f686baf0f44bc1bac1995e5e03691888ca33f
Major-Pr <62181397+Major-Pr@users.noreply.github.com>
2020-07-19 20:27:53 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+
+const greetings = [
+    'i am good you little piece of shit'
+];
+
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.onstart = function () {
+    console.log('voice is activated, but fucc you');
+};
+recognition.onresult = function(event) {
+    const current = event.resultIndex;
+
+    const transcript = event.results[current][0].transcript;
+    content.textContent = transcript;
+    readOutLoud(transcript);
+};
+
+
+//add listener to the btn
+
+btn.addEventListener('click', () => {
+    recognition.start();
+});
+
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+    if(message.includes('how are you')){
+        const finalText= 
+            greetings[Math.floor(Math.random()*greetings.length)];
+        speech.text = finalText;
+    }
+    //speech.text = message;
+    speech.rate = 1;
+    speech.volume = 1;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+}
